68382467c0492af890424942d956ae711f0f0369
weipeng <creatorkuang@gmail.com>
2015-11-28 22:31:58 +0800

    update package info

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'creatorkuang:wechat-share',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'meteor react package for wechat share',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/creatorkuang/react-wechat-share.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
