59828b93eff6ff63fffadfdfc7a03966a3fb1245
weipeng <creatorkuang@gmail.com>
2015-11-28 22:26:24 +0800

    init

---
+++ client/wechatShare.jsx
@@ -0,0 +1,89 @@
+WechatShare=React.createClass({
+	propTypes: {
+	    title:React.PropTypes.string,
+	    desc:React.PropTypes.string,
+	    link:React.PropTypes.string,
+	    imgUrl:React.PropTypes.string,
+	    apis:React.PropTypes.array,
+	    onShareTimeLineSuccess:React.PropTypes.func,
+	    onShareAppMessageSuccess:React.PropTypes.func
+	},
+	getInitialState() {
+	    return {
+	        isWechatReady:false  
+	    };
+	},
+	getDefaultProps() {
+	    return {
+	        apis:['onMenuShareTimeline','onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','onMenuShareQZone']  
+	    };
+	},
+	componentDidMount() {
+		let self=this;
+		function initWechat(data){
+      wx.config({
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId: data.appId, // 必填，公众号的唯一标识
+          timestamp: data.timestamp, // 必填，生成签名的时间戳
+          nonceStr: data.noncestr, // 必填，生成签名的随机串
+          signature: data.signature,// 必填，签名，见附录1
+          jsApiList: self.props.apis // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+      });
+   }
+
+   wx.ready(function(){
+   		self.setState({
+   			isWechatReady:true
+   		})
+
+   		wx.onMenuShareTimeline({
+          title: self.props.title, // 分享标题
+          link: self.props.link, // 分享链接
+          imgUrl: self.props.imgUrl, // 分享图标
+          success: function () { 
+              // 用户确认分享后执行的回调函数
+              if(self.props.onShareTimeLineSuccess){
+              	self.props.onShareTimeLineSuccess()
+              }
+          },
+          cancel: function () { 
+              // 用户取消分享后执行的回调函数
+          }
+      });
+
+      wx.onMenuShareAppMessage({
+        title: self.props.title, // 分享标题
+        desc: self.props.desc, // 分享描述
+        link: self.props.link, // 分享链接
+        imgUrl: self.props.imgUrl, // 分享图标
+        type: '', // 分享类型,music、video或link，不填默认为link
+        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+        success: function () { 
+            // 用户确认分享后执行的回调函数
+            if(self.props.onShareAppMessageSuccess){
+              	self.props.onShareTimeLineSuccess()
+            }
+        },
+        cancel: function () { 
+            // 用户取消分享后执行的回调函数
+        }
+      });
+   })
+
+    let url=this.props.link;
+    Meteor.call('getWechatSinature',url,function(err,result){
+      if(err){
+        alert('创建微信签名失败',err.reason)
+      }
+      if(result){
+        initWechat(result);
+      }else{
+        alert('获取微信签名信息失败');
+      }
+    })
+
+	},
+	render(){
+		return <div></div>
+	}
+})
---
+++ server/method.js
@@ -0,0 +1,69 @@
+Meteor.methods({
+	getWechatSinature:function(url){
+		var self=this;
+		var appId= Meteor.settings.wechat.appid;
+		var appSecret= Meteor.settings.wechat.appSecret;
+		var baseUrl= "https://api.weixin.qq.com/cgi-bin/";
+
+		// get access token
+		var getAccessToken= function(){
+			var url=baseUrl+'token?grant_type=client_credential&appid='+appId+'&secret='+appSecret;
+			var res= request.getSync(url);
+			if(res.response.statusCode==200&&!res.errcode){
+				return JSON.parse(res.body);
+			}else{
+				throw new Meteor.Error("get-token-failed","获取token失败");
+				return;
+			}
+		}
+
+		var getTicket=function(token){
+			var url= baseUrl+"ticket/getticket?access_token="+token+"&type=jsapi";
+			var res= request.getSync(url);
+			if(res.response.statusCode==200){
+				var body=JSON.parse(res.body);
+				if(body.errcode===0){
+					return body.ticket;
+				}else{
+					console.log('ticker 失败',body.errmsg);
+					throw new Meteor.Error("get-ticket-error","获取Ticker失败");
+					return
+				}
+			}else{
+				throw new Meteor.Error("get-token-failed","获取token失败");
+				return;
+			}
+		}
+
+		var createNonceStr= function(){
+			return Math.random().toString(36).substr(2, 15);
+		}
+
+		var createTimeStamp= function(){
+			return parseInt(new Date().getTime() / 1000) + ''		
+		}
+		
+		var createSinature= function(url){
+			var timestamp= createTimeStamp()
+			, 	noncestr= createNonceStr();
+			var token_result= getAccessToken();
+			if(!token_result){
+				throw new Meteor.Error("get-token-error","获取Token失败");
+				return
+			}
+			var access_token=token_result.access_token
+			var jsapi_ticket= getTicket(access_token);
+			var tmpstr= "jsapi_ticket="+jsapi_ticket+"&noncestr="+noncestr+"&timestamp="+timestamp+"&url="+url
+			var signature= CryptoJS.SHA1(tmpstr).toString();
+			var out={
+					appId:appId,
+					timestamp:timestamp,
+					noncestr:noncestr,
+					signature:signature
+			}
+			return out;
+		}
+
+		return createSinature(url);
+	}
+})
