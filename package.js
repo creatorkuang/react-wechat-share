751666c5f4663350c17f53e5d682e1702dd7bb43
weipeng <creatorkuang@gmail.com>
2015-11-30 19:18:29 +0800

    hack weixin location.href for whatever url you want

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'creatorkuang:wechat-share',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'meteor react package for wechat share',
   // URL to the Git repository containing the source code for this package.
